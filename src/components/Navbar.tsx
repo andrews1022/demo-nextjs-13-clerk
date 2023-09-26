cc9be2c92419d2deda89f31f958e12eecfa2161e
Andrew Shearer <andrewshearerdev@gmail.com>
2023-09-25 22:29:30 -0700

    updated middleware and conditionally rendered some nav links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,25 +1,37 @@
-import { UserButton } from "@clerk/nextjs";
+import { currentUser, UserButton } from "@clerk/nextjs";
 import Link from "next/link";
 
-const Navbar = () => {
+import type { User } from "@clerk/nextjs/api";
+
+const Navbar = async () => {
+  const user: User | null = await currentUser();
+
   return (
     <nav>
       <ul>
         <li>
           <Link href="/">Home</Link>
         </li>
+
         <li>
           <Link href="/sign-in">Sign In</Link>
         </li>
+
         <li>
           <Link href="/sign-up">Sign Up</Link>
         </li>
-        <li>
-          <Link href="/profile">Profile</Link>
-        </li>
-        <li>
-          <UserButton afterSignOutUrl="/" />
-        </li>
+
+        {user ? (
+          <>
+            <li>
+              <Link href="/profile">Profile</Link>
+            </li>
+
+            <li>
+              <UserButton afterSignOutUrl="/" />
+            </li>
+          </>
+        ) : null}
       </ul>
     </nav>
   );
